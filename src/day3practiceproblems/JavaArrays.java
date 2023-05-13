864e020022a827d01593e5fe0796b59d5a648ff0
“AratiG“ <aratinalla@gmail.com>
2023-05-13 16:53:52 +0530

    Added: Print elements which are present at even position in array

--- src/day3practiceproblems/JavaArrays.java
+++ src/day3practiceproblems/JavaArrays.java
@@ -3,9 +3,10 @@ package day3practiceproblems;
 public class JavaArrays {
     public static void main(String[] args) {
         int[] arr = {10, 20, 30, 40, 50, 98, 60, 70, 80, 90};
-        //printArray(arr);
+        printArray(arr);
         largestElement(arr);
         smallestElement(arr);
+        evenPosition(arr);
     }
 
     public static void printArray(int[] arr) {
@@ -31,6 +32,12 @@ public class JavaArrays {
                 min = arr[i];
         }
         System.out.println("Smallet element of array: " + min);
+    }
+
+    public static void evenPosition(int[] arr) {
+        System.out.println("Elements which are present on even position:");
+        for (int i = 1; i < arr.length; i = i + 2)
+            System.out.println(arr[i]);
     }
 
 }
