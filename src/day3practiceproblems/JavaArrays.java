3d54c513760347c06b07b97fb44ee7d2088d1a0b
“AratiG“ <aratinalla@gmail.com>
2023-05-13 17:34:12 +0530

    Added: Print array in ascending order

--- src/day3practiceproblems/JavaArrays.java
+++ src/day3practiceproblems/JavaArrays.java
@@ -12,7 +12,8 @@ public class JavaArrays {
         //evenPosition(arr);
         //reverseOrder(arr);
         //oddPosition(arr);
-        duplicateElements(arr);
+        //duplicateElements(arr);
+        ascendingOrder(arr);
     }
 
     public static void printArray(int[] arr) {
@@ -66,6 +67,22 @@ public class JavaArrays {
                     System.out.println(arr[i]);
             }
         }
+    }
+
+    public static void ascendingOrder(int[] arr) {
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println("Array in ascending order :");
+        for (int i = 0; i < arr.length; i++)
+            System.out.println(arr[i]);
     }
 
 }
