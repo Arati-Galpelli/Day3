b2be2d7d537f45576d687db049b4c6445ffe1a4e
“AratiG“ <aratinalla@gmail.com>
2023-05-13 17:04:36 +0530

    Added: Print array in reverse order

--- src/day3practiceproblems/JavaArrays.java
+++ src/day3practiceproblems/JavaArrays.java
@@ -3,10 +3,14 @@ package day3practiceproblems;
 public class JavaArrays {
     public static void main(String[] args) {
         int[] arr = {10, 20, 30, 40, 50, 98, 60, 70, 80, 90};
-        printArray(arr);
-        largestElement(arr);
-        smallestElement(arr);
-        evenPosition(arr);
+        System.out.println("Array Elements:");
+        for (int i = 0; i < arr.length; i++)
+            System.out.println(arr[i]);
+        //printArray(arr);
+        //largestElement(arr);
+        //smallestElement(arr);
+        //evenPosition(arr);
+        reverseOrder(arr);
     }
 
     public static void printArray(int[] arr) {
@@ -37,6 +41,12 @@ public class JavaArrays {
     public static void evenPosition(int[] arr) {
         System.out.println("Elements which are present on even position:");
         for (int i = 1; i < arr.length; i = i + 2)
+            System.out.println(arr[i]);
+    }
+
+    public static void reverseOrder(int[] arr) {
+        System.out.println("Array in reverse order:");
+        for (int i = arr.length - 1; i >= 0; i--)
             System.out.println(arr[i]);
     }
 
