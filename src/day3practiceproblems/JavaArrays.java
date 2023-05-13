9e0bb30d33879d7e44dc35210cef4e12e1f3dd49
“AratiG“ <aratinalla@gmail.com>
2023-05-13 16:33:07 +0530

    Added: Print largest element in Array

--- src/day3practiceproblems/JavaArrays.java
+++ src/day3practiceproblems/JavaArrays.java
@@ -2,8 +2,9 @@ package day3practiceproblems;
 
 public class JavaArrays {
     public static void main(String[] args) {
-        int[] arr = {10, 20, 30, 40, 50, 60, 70, 80, 90};
-        printArray(arr);
+        int[] arr = {10, 20, 30, 40, 50, 98,  60, 70, 80, 90};
+        //printArray(arr);
+        largestElement(arr);
     }
 
     public static void printArray(int[] arr) {
@@ -12,4 +13,14 @@ public class JavaArrays {
             System.out.println(arr[i]);
 
     }
+
+    public static void largestElement(int[] arr) {
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+        System.out.println("Largest element of array: " + max);
+    }
+
 }
