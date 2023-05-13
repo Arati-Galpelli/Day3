48d68dd0d9fd80fbcc59db6019f61fc4972b0955
“AratiG“ <aratinalla@gmail.com>
2023-05-13 17:43:27 +0530

    Added: Print second largest element of array

--- src/day3practiceproblems/JavaArrays.java
+++ src/day3practiceproblems/JavaArrays.java
@@ -13,7 +13,8 @@ public class JavaArrays {
         //reverseOrder(arr);
         //oddPosition(arr);
         //duplicateElements(arr);
-        ascendingOrder(arr);
+        //ascendingOrder(arr);
+        secondLargestElemnet(arr);
     }
 
     public static void printArray(int[] arr) {
@@ -84,5 +85,20 @@ public class JavaArrays {
         for (int i = 0; i < arr.length; i++)
             System.out.println(arr[i]);
     }
+    public static void secondLargestElemnet(int[] arr) {
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println("Second largest element of an array :"+arr[arr.length-2]);
+
+    }
+
 
 }
